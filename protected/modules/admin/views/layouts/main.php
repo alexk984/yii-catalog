b66d5979f3cb6e28e37f363e2d06c9e0825062f6
Alex <alexk984@gmail.com>
2011-08-10 10:57:08 +0400

    refactoring

--- protected/modules/admin/views/layouts/main.php
+++ protected/modules/admin/views/layouts/main.php
@@ -42,9 +42,6 @@
 							array('label' => 'Товары', 'url' => array('/admin/good/')),
 							array('label' => 'Группы Характеристик', 'url' => array('/admin/group/')),
 							array('label' => 'Характеристики товаров', 'url' => array('/admin/attr/')),
-							array('url' => Yii::app()->getModule('user')->loginUrl, 'label' => Yii::app()->getModule('user')->t("Login"), 'visible' => Yii::app()->user->isGuest),
-							array('url' => Yii::app()->getModule('user')->registrationUrl, 'label' => Yii::app()->getModule('user')->t("Register"), 'visible' => Yii::app()->user->isGuest),
-							array('url' => Yii::app()->getModule('user')->profileUrl, 'label' => Yii::app()->getModule('user')->t("Profile"), 'visible' => !Yii::app()->user->isGuest),
 							array('url' => Yii::app()->getModule('user')->logoutUrl, 'label' => Yii::app()->getModule('user')->t("Logout") . ' (' . Yii::app()->user->name . ')', 'visible' => !Yii::app()->user->isGuest),
 						),
 					));
